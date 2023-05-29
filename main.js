f484d59aa90a88921a336c8e2c4942dec3592e87
2011Karthik <100004622+2011Karthik@users.noreply.github.com>
2023-05-29 16:37:36 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -10,7 +10,12 @@ function setup() {
 }
 
 function draw() {
-    background('#969A97')
+    background('#969A97');
+
+    fill('#F90093');
+    stroke('#F90093');
+textSize(10);
+text("Karthik R");    
 }
 
 function modelLoaded() {
@@ -22,5 +27,28 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+    }
+}
+
+
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +"noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = "+ rightWristX + " difference = " + difference);
     }
 }
